2dabdc240ea9617147e71da7c6ea746cb0799743
Sean McMains <sean@mcmains.net>
2017-01-10 22:12:40 -0600

    Morning reminder with custom sound working.

---
+++ 3things/NotificationsManager.swift
@@ -0,0 +1,45 @@
+//
+//  NotificationsManager.swift
+//  3things
+//
+//  Created by Sean Mc Mains on 1/10/17.
+//  Copyright © 2017 Sean McMains. All rights reserved.
+//
+
+import Foundation
+import UserNotifications
+
+class NotificationsManager {
+    private let notificationIdentifier = "morningReminder"
+    
+    func getNotificationPermissions() {
+        let center = UNUserNotificationCenter.current()
+        center.requestAuthorization(options: [.alert, .sound, .badge]) { (granted, error) in
+            // Enable or disable features based on authorization.
+        }
+    }
+    
+    func scheduleReminder() {
+        let content = UNMutableNotificationContent()
+        content.title = "Set Today's Goals"
+        content.body = "It's time to decide what three things are most important for you to accomplish today"
+        content.sound = UNNotificationSound(named: "3things-alert.caf")
+        
+        var dateComponents = DateComponents()
+        dateComponents.hour = 7
+        dateComponents.minute = 0
+        
+        let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: true)
+        
+        let request = UNNotificationRequest(identifier: notificationIdentifier,
+                                            content: content,
+                                            trigger: trigger)
+        
+        let notificationCenter = UNUserNotificationCenter.current()
+        notificationCenter.add(request) { ( error ) in
+            if let error = error {
+                NSLog("Error scheduling notification: \(error.localizedDescription)")
+            }
+        }
+    }
+}
